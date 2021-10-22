503935436397cf688871a99a92dc781e136bbbb6
Juan Pablo <jupgallegopa@gmail.com>
2021-10-22 11:57:26 -0500

    interfaz grafica 1

---
+++ combustible.js
@@ -0,0 +1,9 @@
+let btnVisualizar = document.getElementById('btnvisualizar')
+let btnAgregar = document.getElementById('btnagregar')
+let btnPerecioAlto = document.getElementById('btnprecioalto')
+let btnPrecioBajo = document.getElementById('btnpreciobajo')
+
+btnVisualizar.addEventListener('click')
+btnAgregar.addEventListener('click')
+btnPerecioAlto.addEventListener('click')
+btnPrecioBajo.addEventListener('click')
